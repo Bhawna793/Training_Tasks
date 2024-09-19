833219a0d67aa00a9be9974cfcac4ffb309d9a1c
Bhawna793 <bhawna.bhatia@daffodilsw.com>
2024-09-19 10:04:28 +0530

    training-task

---
+++ script.js
@@ -0,0 +1,14 @@
+
+const user={
+    firstName : 'Adarsh', lastName : 'Singh' , 
+    address : {
+        city : 'Bangalore',
+        pincode : 240303,
+        state : 'Karnataka'
+    },
+
+    moreDeatils : { 
+        phoneNo : 58494949, AadharNo : 9493303
+    }
+
+}
